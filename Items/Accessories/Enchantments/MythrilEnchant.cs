552b2cfe104d4fae9b51375d513ed8fc7ba4a9be
webmilio <therealwebmilio@gmail.com>
2019-11-15 13:01:30 -0500

    enchantments enchantments enchantments enchantments enchantments enchantments enchantments enchantments enchantments enchantments enchantments enchantments enchantments enchantments enchantments enchantments enchantments enchantments enchantments enchantments enchantments enchantments enchantments enchantments enchantments enchantments enchantments

--- Items/Accessories/Enchantments/MythrilEnchant.cs
+++ Items/Accessories/Enchantments/MythrilEnchant.cs
@@ -4,45 +4,35 @@ using Terraria.ModLoader;
 using Terraria.Localization;
 using System.Collections.Generic;
 using Microsoft.Xna.Framework;
+using ThoriumMod.Items.MagicItems;
+using ThoriumMod.Items.NPCItems;
+using ThoriumMod.Items.RangedItems;
 
 namespace FargowiltasSouls.Items.Accessories.Enchantments
 {
-    public class MythrilEnchant : ModItem
+    public class MythrilEnchant : EnchantmentItem
     {
-        private readonly Mod thorium = ModLoader.GetMod("ThoriumMod");
+        public const string TOOLTIP =
+            @"'You feel the knowledge of your weapons seep into your mind'
+20% increased weapon use speed";
+
+
+        public MythrilEnchant() : base("Mythril Enchantment", TOOLTIP, 20, 20,
+            TileID.CrystalBall, Item.sellPrice(gold: 2), ItemRarityID.Pink, new Color(157, 210, 144))
+        {
+        }
+
 
         public override void SetStaticDefaults()
         {
-            DisplayName.SetDefault("Mythril Enchantment");
-            Tooltip.SetDefault(
-@"'You feel the knowledge of your weapons seep into your mind'
-20% increased weapon use speed");
+            base.SetStaticDefaults();
+
             DisplayName.AddTranslation(GameCulture.Chinese, "秘银魔石");
             Tooltip.AddTranslation(GameCulture.Chinese, 
 @"你感觉你对武器的知识渗透到脑海中'
 增加25%武器使用速度");
         }
 
-        public override void ModifyTooltips(List<TooltipLine> list)
-        {
-            foreach (TooltipLine tooltipLine in list)
-            {
-                if (tooltipLine.mod == "Terraria" && tooltipLine.Name == "ItemName")
-                {
-                    tooltipLine.overrideColor = new Color(157, 210, 144);
-                }
-            }
-        }
-
-        public override void SetDefaults()
-        {
-            item.width = 20;
-            item.height = 20;
-            item.accessory = true;
-            ItemID.Sets.ItemNoGravity[item.type] = true;
-            item.rare = 5;
-            item.value = 100000;
-        }
 
         public override void UpdateAccessory(Player player, bool hideVisual)
         {
@@ -50,34 +40,33 @@ namespace FargowiltasSouls.Items.Accessories.Enchantments
                 player.GetModPlayer<FargoPlayer>().AttackSpeed *= 1.2f;
         }
 
-        public override void AddRecipes()
+
+        protected override void AddRecipeBase(ModRecipe recipe)
         {
-            ModRecipe recipe = new ModRecipe(mod);
             recipe.AddRecipeGroup("FargowiltasSouls:AnyMythrilHead");
+
             recipe.AddIngredient(ItemID.MythrilChainmail);
             recipe.AddIngredient(ItemID.MythrilGreaves);
-            
-            if(Fargowiltas.Instance.ThoriumLoaded)
-            {      
-                recipe.AddIngredient(thorium.ItemType("MythrilStaff"));
-                recipe.AddIngredient(ItemID.LaserRifle);
-                recipe.AddIngredient(ItemID.ClockworkAssaultRifle);
-                recipe.AddIngredient(thorium.ItemType("BulletStorm"));
-                recipe.AddIngredient(ItemID.Gatligator);
-                recipe.AddIngredient(ItemID.Megashark);
-                recipe.AddIngredient(thorium.ItemType("Trigun"));  
-            }
-            else
-            {
-                recipe.AddIngredient(ItemID.LaserRifle);
-                recipe.AddIngredient(ItemID.ClockworkAssaultRifle);
-                recipe.AddIngredient(ItemID.Gatligator);
-                recipe.AddIngredient(ItemID.OnyxBlaster);
-            }
-            
-            recipe.AddTile(TileID.CrystalBall);
-            recipe.SetResult(this);
-            recipe.AddRecipe();
+        }
+
+        protected override void AddThoriumRecipe(ModRecipe recipe, Mod thorium)
+        {
+            recipe.AddIngredient(ModContent.ItemType<MythrilStaff>());
+            recipe.AddIngredient(ModContent.ItemType<BulletStorm>());
+            recipe.AddIngredient(ModContent.ItemType<Trigun>());
+
+            recipe.AddIngredient(ItemID.Gatligator);
+            recipe.AddIngredient(ItemID.Megashark);
+            recipe.AddIngredient(ItemID.LaserRifle);
+            recipe.AddIngredient(ItemID.ClockworkAssaultRifle);
+        }
+
+        protected override void FinishRecipeVanilla(ModRecipe recipe)
+        {
+            recipe.AddIngredient(ItemID.LaserRifle);
+            recipe.AddIngredient(ItemID.ClockworkAssaultRifle);
+            recipe.AddIngredient(ItemID.Gatligator);
+            recipe.AddIngredient(ItemID.OnyxBlaster);
         }
     }
 }
--- Items/Accessories/Enchantments/NebulaEnchant.cs
+++ Items/Accessories/Enchantments/NebulaEnchant.cs
@@ -4,77 +4,65 @@ using Terraria.ModLoader;
 using Terraria.Localization;
 using System.Collections.Generic;
 using Microsoft.Xna.Framework;
+using ThoriumMod.Items.Donate;
+using ThoriumMod.Items.Tracker;
 
 namespace FargowiltasSouls.Items.Accessories.Enchantments
 {
-    public class NebulaEnchant : ModItem
+    public class NebulaEnchant : EnchantmentItem
     {
-        private readonly Mod thorium = ModLoader.GetMod("ThoriumMod");
+        public const string TOOLTIP =
+            @"'The pillars of creation have shined upon you'
+Hurting enemies has a chance to spawn buff boosters";
+
+
+        public NebulaEnchant() : base("Nebula Enchantment", TOOLTIP, 20, 20,
+            TileID.LunarCraftingStation, Item.sellPrice(gold: 8), ItemRarityID.Red, new Color(254, 126, 229))
+        {
+        }
+
 
         public override void SetStaticDefaults()
         {
-            DisplayName.SetDefault("Nebula Enchantment");
-            Tooltip.SetDefault(
-@"'The pillars of creation have shined upon you'
-Hurting enemies has a chance to spawn buff boosters");
+            base.SetStaticDefaults();
+
+
             DisplayName.AddTranslation(GameCulture.Chinese, "星云魔石");
             Tooltip.AddTranslation(GameCulture.Chinese, 
 @"'创造之柱照耀着你'
 杀死敌人有概率产生增益效果");
         }
 
-        public override void ModifyTooltips(List<TooltipLine> list)
-        {
-            foreach (TooltipLine tooltipLine in list)
-            {
-                if (tooltipLine.mod == "Terraria" && tooltipLine.Name == "ItemName")
-                {
-                    tooltipLine.overrideColor = new Color(254, 126, 229);
-                }
-            }
-        }
-
-        public override void SetDefaults()
-        {
-            item.width = 20;
-            item.height = 20;
-            item.accessory = true;
-            ItemID.Sets.ItemNoGravity[item.type] = true;
-            item.rare = 10;
-            item.value = 400000;
-        }
 
         public override void UpdateAccessory(Player player, bool hideVisual)
         {
             player.GetModPlayer<FargoPlayer>().NebulaEffect();
         }
 
-        public override void AddRecipes()
+
+        protected override void AddRecipeBase(ModRecipe recipe)
         {
-            ModRecipe recipe = new ModRecipe(mod);
             recipe.AddIngredient(ItemID.NebulaHelmet);
             recipe.AddIngredient(ItemID.NebulaBreastplate);
             recipe.AddIngredient(ItemID.NebulaLeggings);
-            
-            if(Fargowiltas.Instance.ThoriumLoaded)
-            {      
-                recipe.AddIngredient(ItemID.WingsNebula);
-                recipe.AddIngredient(thorium.ItemType("BlackStaff"));
-                recipe.AddIngredient(thorium.ItemType("CatsEye"));
-                recipe.AddIngredient(thorium.ItemType("NebulaReflection"));
-            }
-            else
-            {
-                recipe.AddIngredient(ItemID.ShadowbeamStaff);
-            }
-            
+
             recipe.AddIngredient(ItemID.NebulaArcanum);
             recipe.AddIngredient(ItemID.NebulaBlaze);
             recipe.AddIngredient(ItemID.LunarFlareBook);
+        }
 
-            recipe.AddTile(TileID.LunarCraftingStation);
-            recipe.SetResult(this);
-            recipe.AddRecipe();
+        protected override void AddThoriumRecipe(ModRecipe recipe, Mod thorium)
+        {
+            recipe.AddIngredient(ItemID.WingsNebula);
+
+            recipe.AddIngredient(ModContent.ItemType<BlackStaff>());
+            recipe.AddIngredient(ModContent.ItemType<CatsEye>());
+            recipe.AddIngredient(ModContent.ItemType<NebulaReflection>());
+        }
+
+        protected override void FinishRecipeVanilla(ModRecipe recipe)
+        {
+            recipe.AddIngredient(ItemID.ShadowbeamStaff);
         }
     }
 }
