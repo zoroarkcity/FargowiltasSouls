498a519f4f27ec5a83331531d345feb2b6f8ac28
terrynmuse <darius.loyhrs@hotmail.com>
2019-11-27 10:11:11 -0500

    nerfed hentai spear throw speed yeet

--- Items/Weapons/SwarmDrops/HentaiSpear.cs
+++ Items/Weapons/SwarmDrops/HentaiSpear.cs
@@ -49,8 +49,8 @@ namespace FargowiltasSouls.Items.Weapons.SwarmDrops
             {
                 item.shoot = mod.ProjectileType("HentaiSpearThrown");
                 item.shootSpeed = 25f;
-                item.useAnimation = 90;
-                item.useTime = 90;
+                item.useAnimation = 120;
+                item.useTime = 120;
                 item.thrown = true;
                 item.melee = false;
             }
