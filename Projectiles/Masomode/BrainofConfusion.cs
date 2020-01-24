f2a9b4829717b3f516044639fc0890a87e02a3f0
terrynmuse <darius.loyhrs@hotmail.com>
2020-01-24 14:16:26 -0500

    improved boc confusion telegraph

---
+++ Projectiles/Masomode/BrainofConfusion.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace FargowiltasSouls.Projectiles.Masomode
+{
+    public class BrainofConfusion : ModProjectile
+    {
+        public override string Texture => "Terraria/Projectile_565";
+
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Brain of Confusion"); 
+        }
+
+        public override void SetDefaults()
+        {
+            projectile.CloneDefaults(ProjectileID.BrainOfConfusion);
+            projectile.scale = 6f;
+            aiType = ProjectileID.BrainOfConfusion;
+        }
+
+        public override Color? GetAlpha(Color lightColor)
+        {
+            return Color.White * 0.5f;
+        }
+    }
+}
