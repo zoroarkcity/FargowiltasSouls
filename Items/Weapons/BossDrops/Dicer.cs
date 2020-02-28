0c077304a313da5436e6e57b324523d4b7ba3b80
Terry N. Muse <darius.loyhrs@hotmail.com>
2020-02-28 16:48:25 -0500

    nerfed dicer

--- Items/Weapons/BossDrops/Dicer.cs
+++ Items/Weapons/BossDrops/Dicer.cs
@@ -32,7 +32,7 @@ namespace FargowiltasSouls.Items.Weapons.BossDrops
             item.useTime = 25;
             item.shootSpeed = 16f;
             item.knockBack = 2.5f;
-            item.damage = 100;
+            item.damage = 80;
             item.value = Item.sellPrice(0, 30);
             item.rare = 8;
         }
