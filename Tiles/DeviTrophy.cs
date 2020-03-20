d0e407a1bf88b8951252d488e5f20f04a4c9b849
Terry N. Muse <darius.loyhrs@hotmail.com>
2020-03-20 12:35:57 -0400

    added devi trophy

---
+++ Tiles/DeviTrophy.cs
@@ -0,0 +1,32 @@
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ModLoader;
+using Terraria.ObjectData;
+
+namespace FargowiltasSouls.Tiles
+{
+    public class DeviTrophy : ModTile
+    {
+        public override void SetDefaults()
+        {
+            Main.tileFrameImportant[Type] = true;
+            Main.tileLavaDeath[Type] = true;
+
+            TileObjectData.newTile.CopyFrom(TileObjectData.Style3x3Wall);
+            TileObjectData.newTile.StyleHorizontal = true;
+            TileObjectData.newTile.StyleWrapLimit = 36;
+            TileObjectData.addTile(Type);
+
+            disableSmartCursor = true;
+
+            ModTranslation name = CreateMapEntryName();
+            name.SetDefault("Deviantt Trophy");
+            AddMapEntry(new Color(120, 85, 60), name);
+        }
+
+        public override void KillMultiTile(int i, int j, int frameX, int frameY)
+		{
+			Item.NewItem(i * 16, j * 16, 48, 48, mod.ItemType("DeviTrophy"));
+		}
+    }
+}
