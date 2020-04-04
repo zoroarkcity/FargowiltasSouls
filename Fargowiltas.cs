c674b0592200970527d022e464ded1b04472bed0
Terry N. Muse <darius.loyhrs@hotmail.com>
2020-04-04 14:11:59 -0400

    added banner recipe for baby guardian summon adjusted recipe for jungle chest/rune orb to use deviating energy

--- Fargowiltas.cs
+++ Fargowiltas.cs
@@ -827,16 +827,23 @@ namespace FargowiltasSouls
             ModRecipe recipe = new ModRecipe(this);
             recipe.AddIngredient(ItemID.SoulofLight, 7);
             recipe.AddIngredient(ItemID.SoulofNight, 7);
-            recipe.AddIngredient(ItemType("VolatileEnergy"));
+            recipe.AddIngredient(ModContent.ItemType<Items.Misc.DeviatingEnergy>(), 5);
             recipe.AddTile(TileID.MythrilAnvil);
             recipe.SetResult(ModContent.ItemType<JungleChest>());
             recipe.AddRecipe();
 
             recipe = new ModRecipe(this);
             recipe.AddIngredient(ItemID.WizardHat);
-            recipe.AddIngredient(ItemType("VolatileEnergy"), 5);
+            recipe.AddIngredient(ModContent.ItemType<Items.Misc.DeviatingEnergy>(), 5);
             recipe.AddTile(TileID.MythrilAnvil);
             recipe.SetResult(ModContent.ItemType<RuneOrb>());
+            recipe.AddRecipe();
+
+            recipe = new ModRecipe(this);
+            recipe.AddRecipeGroup("FargowiltasSouls:AnyBonesBanner", 2);
+            recipe.AddIngredient(ModContent.ItemType<Items.Misc.DeviatingEnergy>(), 5);
+            recipe.AddTile(TileID.Anvils);
+            recipe.SetResult(ModContent.ItemType<InnocuousSkull>());
             recipe.AddRecipe();
         }
 
@@ -845,6 +852,10 @@ namespace FargowiltasSouls
             //drax
             RecipeGroup group = new RecipeGroup(() => Lang.misc[37] + " Drax", ItemID.Drax, ItemID.PickaxeAxe);
             RecipeGroup.RegisterGroup("FargowiltasSouls:AnyDrax", group);
+
+            //dungeon enemies
+            group = new RecipeGroup(() => Lang.misc[37] + " Angry or Armored Bones Banner", ItemID.AngryBonesBanner, ItemID.BlueArmoredBonesBanner, ItemID.HellArmoredBonesBanner, ItemID.RustyArmoredBonesBanner);
+            RecipeGroup.RegisterGroup("FargowiltasSouls:AnyBonesBanner", group);
 
             //cobalt
             group = new RecipeGroup(() => Lang.misc[37] + " Cobalt Repeater", ItemID.CobaltRepeater, ItemID.PalladiumRepeater);
