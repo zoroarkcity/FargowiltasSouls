cf988daac3fb84a55923da330afe799819403822
Terry N. Muse <darius.loyhrs@hotmail.com>
2020-04-09 23:13:05 -0400

    nerfed spaz's p3 +25% speed bonus to +10%

--- NPCs/EBossAI.cs
+++ NPCs/EBossAI.cs
@@ -1393,6 +1393,8 @@ namespace FargowiltasSouls.NPCs
             }
             else
             {
+                npc.position += npc.velocity / 10f;
+
                 if (npc.ai[1] == 0f) //not dashing
                 {
                     if (retiAlive && (Main.npc[retiBoss].ai[0] < 4f || Main.npc[retiBoss].GetGlobalNPC<EModeGlobalNPC>().Counter == 0)) //reti is in normal AI
