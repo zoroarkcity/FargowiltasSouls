9ba301cafcc98154a47ded42fcdb41b60ab7be5e
Supreme Catastrlysm <39974170+He1P-SiXbOnEs@users.noreply.github.com>
2020-04-30 11:46:09 +0800

    Update SulphurousEnchant.cs

--- Items/Accessories/Enchantments/Calamity/SulphurousEnchant.cs
+++ Items/Accessories/Enchantments/Calamity/SulphurousEnchant.cs
@@ -1,6 +1,7 @@
 ﻿using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
+using Terraria.Localization;
 using CalamityMod.Items.Armor;
 using CalamityMod.Items.Accessories;
 using CalamityMod.Items.Weapons.Rogue;
