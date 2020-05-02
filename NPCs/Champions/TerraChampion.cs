8d29256b7b815a673b09335d85b577270ee6da65
Terry N. Muse <darius.loyhrs@hotmail.com>
2020-05-02 17:44:33 -0400

    reduxed DR on terra p2

--- NPCs/Champions/TerraChampion.cs
+++ NPCs/Champions/TerraChampion.cs
@@ -671,7 +671,7 @@ namespace FargowiltasSouls.NPCs.Champions
             if (resist)
                 damage /= 10;
             if (npc.life < npc.lifeMax / 10)
-                damage /= 5;
+                damage /= 4;
             return true;
         }
 
