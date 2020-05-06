a7bb18994fd6d6f2547db5e47cdefbb74f4b0d50
Terry N. Muse <darius.loyhrs@hotmail.com>
2020-05-06 15:05:11 -0400

    polished cosmos

--- Projectiles/Champions/CosmosMeteor2.cs
+++ Projectiles/Champions/CosmosMeteor2.cs
@@ -55,100 +55,6 @@ namespace FargowiltasSouls.Projectiles.Champions
                 projectile.rotation += (float)Math.PI / 2;
                 if (projectile.ai[0] < 0)
                     projectile.rotation += (float)Math.PI;
-
-                Vector2 size = new Vector2(500, 500);
-                Vector2 spawnPos = projectile.Center;
-                spawnPos.X -= size.X / 2;
-                spawnPos.Y -= size.Y / 2;
-
-                for (int num615 = 0; num615 < 45; num615++)
-                {
-                    int num616 = Dust.NewDust(spawnPos, (int)size.X, (int)size.Y, 31, 0f, 0f, 100, default(Color), 1.5f);
-                    Main.dust[num616].velocity *= 1.4f;
-                }
-
-                for (int num617 = 0; num617 < 60; num617++)
-                {
-                    int num618 = Dust.NewDust(spawnPos, (int)size.X, (int)size.Y, DustID.Fire, 0f, 0f, 100, default(Color), 3.5f);
-                    Main.dust[num618].noGravity = true;
-                    Main.dust[num618].velocity *= 7f;
-                    num618 = Dust.NewDust(spawnPos, (int)size.X, (int)size.Y, DustID.Fire, 0f, 0f, 100, default(Color), 1.5f);
-                    Main.dust[num618].velocity *= 3f;
-                }
-
-                for (int num619 = 0; num619 < 3; num619++)
-                {
-                    float scaleFactor9 = 0.4f;
-                    if (num619 == 1) scaleFactor9 = 0.8f;
-                    int num620 = Gore.NewGore(projectile.Center, default(Vector2), Main.rand.Next(61, 64));
-                    Main.gore[num620].velocity *= scaleFactor9;
-                    Gore gore97 = Main.gore[num620];
-                    gore97.velocity.X = gore97.velocity.X + 1f;
-                    Gore gore98 = Main.gore[num620];
-                    gore98.velocity.Y = gore98.velocity.Y + 1f;
-                    num620 = Gore.NewGore(projectile.Center, default(Vector2), Main.rand.Next(61, 64));
-                    Main.gore[num620].velocity *= scaleFactor9;
-                    Gore gore99 = Main.gore[num620];
-                    gore99.velocity.X = gore99.velocity.X - 1f;
-                    Gore gore100 = Main.gore[num620];
-                    gore100.velocity.Y = gore100.velocity.Y + 1f;
-                    num620 = Gore.NewGore(projectile.Center, default(Vector2), Main.rand.Next(61, 64));
-                    Main.gore[num620].velocity *= scaleFactor9;
-                    Gore gore101 = Main.gore[num620];
-                    gore101.velocity.X = gore101.velocity.X + 1f;
-                    Gore gore102 = Main.gore[num620];
-                    gore102.velocity.Y = gore102.velocity.Y - 1f;
-                    num620 = Gore.NewGore(projectile.Center, default(Vector2), Main.rand.Next(61, 64));
-                    Main.gore[num620].velocity *= scaleFactor9;
-                    Gore gore103 = Main.gore[num620];
-                    gore103.velocity.X = gore103.velocity.X - 1f;
-                    Gore gore104 = Main.gore[num620];
-                    gore104.velocity.Y = gore104.velocity.Y - 1f;
-                }
-
-
-                for (int k = 0; k < 40; k++) //make visual dust
-                {
-                    Vector2 dustPos = projectile.position;
-                    dustPos.X += Main.rand.Next(projectile.width);
-                    dustPos.Y += Main.rand.Next(projectile.height);
-
-                    for (int i = 0; i < 30; i++)
-                    {
-                        int dust = Dust.NewDust(dustPos, 32, 32, 31, 0f, 0f, 100, default(Color), 3f);
-                        Main.dust[dust].velocity *= 1.4f;
-                    }
-
-                    for (int i = 0; i < 20; i++)
-                    {
-                        int dust = Dust.NewDust(dustPos, 32, 32, DustID.Fire, 0f, 0f, 100, default(Color), 3.5f);
-                        Main.dust[dust].noGravity = true;
-                        Main.dust[dust].velocity *= 7f;
-                        dust = Dust.NewDust(dustPos, 32, 32, DustID.Fire, 0f, 0f, 100, default(Color), 1.5f);
-                        Main.dust[dust].velocity *= 3f;
-                    }
-
-                    float scaleFactor9 = 0.5f;
-                    for (int j = 0; j < 4; j++)
-                    {
-                        int gore = Gore.NewGore(dustPos, default(Vector2), Main.rand.Next(61, 64));
-                        Main.gore[gore].velocity *= scaleFactor9;
-                        Main.gore[gore].velocity.X += 1f;
-                        Main.gore[gore].velocity.Y += 1f;
-                    }
-                }
-
-
-                const int num226 = 80;
-                for (int num227 = 0; num227 < num226; num227++)
-                {
-                    Vector2 vector6 = Vector2.UnitX * 40f;
-                    vector6 = vector6.RotatedBy(((num227 - (num226 / 2 - 1)) * 6.28318548f / num226), default(Vector2)) + projectile.Center;
-                    Vector2 vector7 = vector6 - projectile.Center;
-                    int num228 = Dust.NewDust(vector6 + vector7, 0, 0, DustID.Fire, 0f, 0f, 0, default(Color), 3f);
-                    Main.dust[num228].noGravity = true;
-                    Main.dust[num228].velocity = vector7;
-                }
             }
 
             int ai1 = (int)projectile.ai[1];
@@ -160,13 +66,9 @@ namespace FargowiltasSouls.Projectiles.Champions
 
             projectile.timeLeft = 2;
             
-            const float maxAmplitude = 1000;
-            const float minDistance = 200;
+            const float maxAmplitude = 850;
             float offset = Math.Abs(maxAmplitude * (float)Math.Sin(Main.npc[ai1].ai[2] * 2 * (float)Math.PI / 300));
-            if (offset < minDistance)
-            {
-                offset = minDistance - (minDistance - offset) / 2;
-            }
+            offset += 150;
             projectile.rotation += 0.01f;
             projectile.Center = Main.npc[ai1].Center + offset * projectile.rotation.ToRotationVector2();
         }
@@ -228,9 +130,9 @@ namespace FargowiltasSouls.Projectiles.Champions
 
             for (int k = 0; k < 40; k++) //make visual dust
             {
-                Vector2 dustPos = projectile.position;
-                dustPos.X += Main.rand.Next(projectile.width);
-                dustPos.Y += Main.rand.Next(projectile.height);
+                Vector2 dustPos = spawnPos;
+                dustPos.X += Main.rand.Next((int)size.X);
+                dustPos.Y += Main.rand.Next((int)size.Y);
 
                 for (int i = 0; i < 30; i++)
                 {
