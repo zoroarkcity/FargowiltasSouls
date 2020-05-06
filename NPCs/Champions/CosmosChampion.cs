3eeed1e1ee5a32f0301bf7862320ba0dd443bed5
Terry N. Muse <darius.loyhrs@hotmail.com>
2020-05-06 15:09:08 -0400

    last dust tweak

--- NPCs/Champions/CosmosChampion.cs
+++ NPCs/Champions/CosmosChampion.cs
@@ -174,6 +174,88 @@ namespace FargowiltasSouls.NPCs.Champions
                             Projectile.NewProjectile(npc.Center, Vector2.Zero, ModContent.ProjectileType<CosmosMeteor2>(), npc.damage / 4, 0f, Main.myPlayer, 1, npc.whoAmI);
                             Projectile.NewProjectile(npc.Center, Vector2.Zero, ModContent.ProjectileType<CosmosMeteor2>(), npc.damage / 4, 0f, Main.myPlayer, -1, npc.whoAmI);
                         }
+
+                        Vector2 size = new Vector2(500, 500);
+                        Vector2 spawnPos = npc.Center;
+                        spawnPos.X -= size.X / 2;
+                        spawnPos.Y -= size.Y / 2;
+
+                        for (int num615 = 0; num615 < 30; num615++)
+                        {
+                            int num616 = Dust.NewDust(spawnPos, (int)size.X, (int)size.Y, 31, 0f, 0f, 100, default(Color), 1.5f);
+                            Main.dust[num616].velocity *= 1.4f;
+                        }
+
+                        for (int num617 = 0; num617 < 50; num617++)
+                        {
+                            int num618 = Dust.NewDust(spawnPos, (int)size.X, (int)size.Y, DustID.Fire, 0f, 0f, 100, default(Color), 3.5f);
+                            Main.dust[num618].noGravity = true;
+                            Main.dust[num618].velocity *= 7f;
+                            num618 = Dust.NewDust(spawnPos, (int)size.X, (int)size.Y, DustID.Fire, 0f, 0f, 100, default(Color), 1.5f);
+                            Main.dust[num618].velocity *= 3f;
+                        }
+
+                        for (int num619 = 0; num619 < 2; num619++)
+                        {
+                            float scaleFactor9 = 0.4f;
+                            if (num619 == 1) scaleFactor9 = 0.8f;
+                            int num620 = Gore.NewGore(npc.Center, default(Vector2), Main.rand.Next(61, 64));
+                            Main.gore[num620].velocity *= scaleFactor9;
+                            Gore gore97 = Main.gore[num620];
+                            gore97.velocity.X = gore97.velocity.X + 1f;
+                            Gore gore98 = Main.gore[num620];
+                            gore98.velocity.Y = gore98.velocity.Y + 1f;
+                            num620 = Gore.NewGore(npc.Center, default(Vector2), Main.rand.Next(61, 64));
+                            Main.gore[num620].velocity *= scaleFactor9;
+                            Gore gore99 = Main.gore[num620];
+                            gore99.velocity.X = gore99.velocity.X - 1f;
+                            Gore gore100 = Main.gore[num620];
+                            gore100.velocity.Y = gore100.velocity.Y + 1f;
+                            num620 = Gore.NewGore(npc.Center, default(Vector2), Main.rand.Next(61, 64));
+                            Main.gore[num620].velocity *= scaleFactor9;
+                            Gore gore101 = Main.gore[num620];
+                            gore101.velocity.X = gore101.velocity.X + 1f;
+                            Gore gore102 = Main.gore[num620];
+                            gore102.velocity.Y = gore102.velocity.Y - 1f;
+                            num620 = Gore.NewGore(npc.Center, default(Vector2), Main.rand.Next(61, 64));
+                            Main.gore[num620].velocity *= scaleFactor9;
+                            Gore gore103 = Main.gore[num620];
+                            gore103.velocity.X = gore103.velocity.X - 1f;
+                            Gore gore104 = Main.gore[num620];
+                            gore104.velocity.Y = gore104.velocity.Y - 1f;
+                        }
+
+
+                        for (int k = 0; k < 20; k++) //make visual dust
+                        {
+                            Vector2 dustPos = spawnPos;
+                            dustPos.X += Main.rand.Next((int)size.X);
+                            dustPos.Y += Main.rand.Next((int)size.Y);
+
+                            for (int i = 0; i < 20; i++)
+                            {
+                                int dust = Dust.NewDust(dustPos, 32, 32, 31, 0f, 0f, 100, default(Color), 3f);
+                                Main.dust[dust].velocity *= 1.4f;
+                            }
+
+                            for (int i = 0; i < 10; i++)
+                            {
+                                int dust = Dust.NewDust(dustPos, 32, 32, DustID.Fire, 0f, 0f, 100, default(Color), 3.5f);
+                                Main.dust[dust].noGravity = true;
+                                Main.dust[dust].velocity *= 7f;
+                                dust = Dust.NewDust(dustPos, 32, 32, DustID.Fire, 0f, 0f, 100, default(Color), 1.5f);
+                                Main.dust[dust].velocity *= 3f;
+                            }
+
+                            float scaleFactor9 = 0.5f;
+                            for (int j = 0; j < 2; j++)
+                            {
+                                int gore = Gore.NewGore(dustPos, default(Vector2), Main.rand.Next(61, 64));
+                                Main.gore[gore].velocity *= scaleFactor9;
+                                Main.gore[gore].velocity.X += 1f;
+                                Main.gore[gore].velocity.Y += 1f;
+                            }
+                        }
                     }
 
                     if (++npc.ai[2] > 300 || npc.ai[2] == 150)
@@ -263,10 +345,10 @@ namespace FargowiltasSouls.NPCs.Champions
                             }
                         }
 
-                        const int num226 = 120;
+                        const int num226 = 150;
                         for (int num227 = 0; num227 < num226; num227++)
                         {
-                            Vector2 vector6 = Vector2.UnitX * 40f;
+                            Vector2 vector6 = Vector2.UnitX * 60f;
                             vector6 = vector6.RotatedBy(((num227 - (num226 / 2 - 1)) * 6.28318548f / num226), default(Vector2)) + npc.Center;
                             Vector2 vector7 = vector6 - npc.Center;
                             int num228 = Dust.NewDust(vector6 + vector7, 0, 0, type, 0f, 0f, 0, default(Color), 3f);
