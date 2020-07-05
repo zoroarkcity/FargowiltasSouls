a401cb8dec547a7a938056ea87ecbf9553f4dce0
Terry N. Muse <darius.loyhrs@hotmail.com>
2020-07-05 12:35:09 -0400

    first buffs

--- Items/Weapons/SwarmDrops/Blender.cs
+++ Items/Weapons/SwarmDrops/Blender.cs
@@ -33,7 +33,7 @@ namespace FargowiltasSouls.Items.Weapons.SwarmDrops
 			item.useTime = 25;
 			item.shootSpeed = 16f;
 			item.knockBack = 2.5f;
-			item.damage = 220;
+			item.damage = 300;
 
             item.value = Item.sellPrice(0, 25);
             item.rare = 11;
