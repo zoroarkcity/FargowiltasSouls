633d73139f9b14edbd6a44a0415a2fba4717cb27
Terry N. Muse <darius.loyhrs@hotmail.com>
2020-07-07 14:34:27 -0400

    mutant DR 10 -> 20

--- Items/Weapons/SwarmDrops/OpticStaffEX.cs
+++ Items/Weapons/SwarmDrops/OpticStaffEX.cs
@@ -18,7 +18,7 @@ namespace FargowiltasSouls.Items.Weapons.SwarmDrops
 
         public override void SetDefaults()
         {
-            item.damage = 280;
+            item.damage = 275;
             item.mana = 10;
             item.summon = true;
             item.width = 24;
