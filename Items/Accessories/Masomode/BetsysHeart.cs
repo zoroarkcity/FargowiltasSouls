30644d038227e7cfa0790a82692811f0961d0de4
Supreme Catastrlysm <39974170+He1P-SiXbOnEs@users.noreply.github.com>
2020-08-25 13:11:00 +0800

    Update Chinese Translation

--- Items/Accessories/Masomode/BetsysHeart.cs
+++ Items/Accessories/Masomode/BetsysHeart.cs
@@ -17,7 +17,8 @@ Press the Fireball Dash key to perform a short invincible dash
             DisplayName.AddTranslation(GameCulture.Chinese, "贝特希之心");
             Tooltip.AddTranslation(GameCulture.Chinese, @"'微烤,五分熟'
 免疫渗出,枯萎武器和枯萎盔甲
-暴击造成贝特希的诅咒");
+暴击造成贝特希的诅咒
+按下火球冲刺按键来进行一次短程的无敌冲刺");
         }
 
         public override void SetDefaults()
