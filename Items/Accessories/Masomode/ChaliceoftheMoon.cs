459f9cea79851109b653d2c553af17aed102417b
Supreme Catastrlysm <39974170+He1P-SiXbOnEs@users.noreply.github.com>
2020-08-25 13:19:45 +0800

    Update Chinese Translation

--- Items/Accessories/Masomode/ChaliceoftheMoon.cs
+++ Items/Accessories/Masomode/ChaliceoftheMoon.cs
@@ -21,7 +21,7 @@ Summons a friendly Cultist and plant to fight at your side
 'The moon smiles'");
             DisplayName.AddTranslation(GameCulture.Chinese, "月之杯");
             Tooltip.AddTranslation(GameCulture.Chinese, @"月亮的微笑
-免疫毒液, 常春藤毒, 燃烧, 导火线, 死亡标记和着迷
+免疫毒液, 常春藤毒, 燃烧, 导火线, 低地和死亡标记
 免疫蜂群, 萎缩, 卡壳, 反魔力流和反社交
 增加生命回复
 在空中按'下'键快速下落
