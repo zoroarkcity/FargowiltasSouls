598a920e9a6a542ed8463602a72ec07e12e9a2c0
Terry N. Muse <darius.loyhrs@hotmail.com>
2020-08-26 12:34:00 -0400

    rain cloud will only absorb friendly projectiles that inflict damage (should make absolute sure it can't eat boss arenas) rain cloud will not absorb minions

--- Projectiles/Souls/RainCloud.cs
+++ Projectiles/Souls/RainCloud.cs
@@ -41,7 +41,8 @@ namespace FargowiltasSouls.Projectiles.Masomode
             {
                 Projectile proj = Main.projectile[i];
 
-                if (proj.active && !proj.hostile && proj.owner == player.whoAmI && proj.type != projectile.type && proj.type != ProjectileID.RainFriendly && proj.Hitbox.Intersects(projectile.Hitbox))
+                if (proj.active && proj.friendly && !proj.hostile && proj.owner == player.whoAmI && proj.damage > 0 && !proj.minion
+                    && proj.type != projectile.type && proj.type != ProjectileID.RainFriendly && proj.Hitbox.Intersects(projectile.Hitbox))
                 {
                     if (projectile.scale < 3f)
                     {
