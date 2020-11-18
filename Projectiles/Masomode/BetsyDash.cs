3be04f364f6712256183880b6834adbb133f84c7
zoroarkcity <zoroarkcity2002@gmail.com>
2020-11-17 23:47:59 -0800

    fireball dash consistent with new drawcode

--- Projectiles/Masomode/BetsyDash.cs
+++ Projectiles/Masomode/BetsyDash.cs
@@ -14,7 +14,7 @@ namespace FargowiltasSouls.Projectiles.Masomode
         public override void SetStaticDefaults()
         {
             DisplayName.SetDefault("Dash");
-            ProjectileID.Sets.TrailCacheLength[projectile.type] = 6;
+            ProjectileID.Sets.TrailCacheLength[projectile.type] = 10;
             ProjectileID.Sets.TrailingMode[projectile.type] = 2;
         }
 
@@ -26,6 +26,7 @@ namespace FargowiltasSouls.Projectiles.Masomode
             projectile.friendly = true;
             projectile.melee = true;
             projectile.ignoreWater = true;
+            projectile.alpha = 60;
             projectile.timeLeft = 15;
             projectile.penetrate = -1;
             projectile.GetGlobalProjectile<FargoGlobalProjectile>().CanSplit = false;
@@ -103,17 +104,10 @@ namespace FargowiltasSouls.Projectiles.Masomode
             return false; //dont kill proj when hits tiles
         }
 
-        public override Color? GetAlpha(Color lightColor)
-        {
-            return Color.White;
-        }
-
         public override bool PreDraw(SpriteBatch spriteBatch, Color lightColor)
         {
             if (projectile.localAI[0] != 0)
             {
-                Vector2 offset = projectile.velocity == Vector2.Zero ? Vector2.Zero : Vector2.Normalize(projectile.velocity) * 25f;
-
                 Texture2D texture2D13 = Main.projectileTexture[projectile.type];
                 int num156 = Main.projectileTexture[projectile.type].Height / Main.projFrames[projectile.type]; //ypos of lower right corner of sprite to draw
                 int y3 = num156 * projectile.frame; //ypos of upper left corner of sprite to draw
@@ -121,17 +115,28 @@ namespace FargowiltasSouls.Projectiles.Masomode
                 Vector2 origin2 = rectangle.Size() / 2f;
 
                 Color color26 = Color.White;
+                color26 = projectile.GetAlpha(color26);
+                color26.A = (byte)projectile.alpha;
+
+                SpriteEffects effects = projectile.spriteDirection < 0 ? SpriteEffects.None : SpriteEffects.FlipHorizontally;
 
                 for (int i = 0; i < ProjectileID.Sets.TrailCacheLength[projectile.type]; i++)
                 {
-                    Color color27 = color26;
+                    float lerpamount = 0;
+                    if (i > 3 && i < 5)
+                        lerpamount = 0.6f;
+                    if (i >= 5)
+                        lerpamount = 0.8f;
+
+                    Color color27 = Color.Lerp(Color.White, Color.Purple, lerpamount) * 0.75f * 0.5f;
                     color27 *= (float)(ProjectileID.Sets.TrailCacheLength[projectile.type] - i) / ProjectileID.Sets.TrailCacheLength[projectile.type];
+                    float scale = projectile.scale * (float)(ProjectileID.Sets.TrailCacheLength[projectile.type] - i) / ProjectileID.Sets.TrailCacheLength[projectile.type];
                     Vector2 value4 = projectile.oldPos[i];
                     float num165 = projectile.oldRot[i];
-                    Main.spriteBatch.Draw(texture2D13, value4 - offset + projectile.Size / 2f - Main.screenPosition + new Vector2(0, projectile.gfxOffY), new Microsoft.Xna.Framework.Rectangle?(rectangle), color27, num165, origin2, projectile.scale, SpriteEffects.None, 0f);
+                    Main.spriteBatch.Draw(texture2D13, value4 + projectile.Size / 2f - Main.screenPosition + new Vector2(0, projectile.gfxOffY), new Microsoft.Xna.Framework.Rectangle?(rectangle), color27, num165, origin2, scale, effects, 0f);
                 }
 
-                Main.spriteBatch.Draw(texture2D13, projectile.Center - offset - Main.screenPosition + new Vector2(0f, projectile.gfxOffY), new Microsoft.Xna.Framework.Rectangle?(rectangle), projectile.GetAlpha(lightColor), projectile.rotation, origin2, projectile.scale, SpriteEffects.None, 0f);
+                Main.spriteBatch.Draw(texture2D13, projectile.Center - Main.screenPosition + new Vector2(0f, projectile.gfxOffY), new Microsoft.Xna.Framework.Rectangle?(rectangle), color26, projectile.rotation, origin2, projectile.scale, effects, 0f);
             }
             return false;
         }
