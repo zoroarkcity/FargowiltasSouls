7447ed524937a22b42ab132bfa48cf6bb37ec0eb
Terry N. Muse <darius.loyhrs@hotmail.com>
2021-01-09 13:35:42 -0500

    ml p2 vortex bolts have slight random variation

--- Projectiles/Masomode/MoonLordVortex.cs
+++ Projectiles/Masomode/MoonLordVortex.cs
@@ -41,7 +41,7 @@ namespace FargowiltasSouls.Projectiles.Masomode
                             {
                                 projectile.localAI[1] = 2;
 
-                                Vector2 dir = projectile.DirectionTo(player.Center);
+                                Vector2 dir = projectile.DirectionTo(player.Center).RotatedByRandom(MathHelper.ToRadians(10));
                                 float ai1New = (Main.rand.Next(2) == 0) ? 1 : -1; //randomize starting direction
                                 Vector2 vel = Vector2.Normalize(dir) * 6f;
                                 Projectile.NewProjectile(projectile.Center, vel * 6, ModContent.ProjectileType<Champions.CosmosLightning>(),
